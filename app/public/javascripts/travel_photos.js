6828a430ce96fec7a7b3754e16463b71f5d15f21
David Mulvihill <davidmulvihill@outlook.com>
2024-06-17 22:18:49 -0500

    pushing latest changes

--- app/public/javascripts/travel_photos.js
+++ app/public/javascripts/travel_photos.js
@@ -1,5 +1,5 @@
 const previous_button = document.getElementById("prev");
-let slideIndex = 1;
+let slideIndex = 0;
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -10,27 +10,29 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-
-    console.log(slideIndex);
     let i;
     let slides = document.getElementsByClassName("travel-slideshow");
+
+
     let dots = document.getElementsByClassName("dot");
-    console.log(slides.length);
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active", "");        
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-
-    let x = document.getElementById("travel-text-title").innerHTML = "Panama";
-
+    if (id_kara === true) {
+        document.getElementById("travel-text-title").innerHTML = "Karakoram Highway";
+    }else if (id_india === true) {
+        document.getElementById("travel-text-title").innerHTML = "India";
+    } else if (id_pan === true) {
+        document.getElementById("travel-text-title").innerHTML = "Panama";
+    }
 }
-
 
 showSlides(slideIndex);
 previous_button.addEventListener("onclick", plusSlides(-1));
